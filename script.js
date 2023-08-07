7199e1164c1828e8815a632adaa6689f6409d585
Daniel Bindig <d.bindig@outlook.com>
2023-08-07 21:01:40 +0930

    Making commit to fix bugs

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-function init() {
-    var searchButton = document.getElementById('btn')
-    searchButton.addEventListener('click', fetchWeather());
 
-    function fetchWeather() {
-        let city = document.getElementById('city-search').value;
-        let key = 'e99ffb98e40309894b1ddec2b52687ff';
-        let units = 'metric';
-        let url = `api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${key}&units${units}`;
-        //fetch
-        fetch(url)
-            .then(function (response) {
-                return response.json();
-            })
-            .then(function (data) {
-                console.log(data);
-            })
+var searchButton = document.getElementById('btn')
 
-        
-    }
-
-    
+function fetchLocation() {
+    let city = document.getElementById('city-search').value;
+    let key = 'e99ffb98e40309894b1ddec2b52687ff';
+    let locationUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${key}`;
+    //fetch
+    fetch(locationUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            
+        })
 }
 
-init()
+function showWeather(response) {
+    let row = document.querySelector('.days.row')
+    row.innerHTML = response.daily.map(function(day) {
+        return '<p>Day<p>'
+    }).join(' ');
+    console.log(response.lon);
+}
+searchButton.addEventListener('search', fetchLocation())
