63283a6a38db5d7c042c631b1822c989e3f5b927
Daniel Bindig <d.bindig@outlook.com>
2023-08-07 22:39:25 +0930

    Added API call for longitude and latitude. Also cleaned up the code.

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-
+const key = 'e99ffb98e40309894b1ddec2b52687ff';
 var searchButton = document.getElementById('btn')
 
 function fetchLocation() {
     let city = document.getElementById('city-search').value;
-    let key = 'e99ffb98e40309894b1ddec2b52687ff';
+    getCoordinates(city)
+}
+
+function getCoordinates(city){
     let locationUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${key}`;
     //fetch
     fetch(locationUrl)
@@ -11,16 +14,29 @@ function fetchLocation() {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
-            
+            console.log(data[0]);
+            var lat = data[0].lat;
+            var lon = data[0].lon;
+            getWeather(lat, lon)
         })
 }
 
-function showWeather(response) {
+function getWeather(lat, lon) {
+    let weatherUrl = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${key}`
+        fetch(weatherUrl)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data)
+                showWeather(data)
+            })
+}
+
+function showWeather(data) {
     let row = document.querySelector('.days.row')
     row.innerHTML = response.daily.map(function(day) {
         return '<p>Day<p>'
     }).join(' ');
-    console.log(response.lon);
 }
 searchButton.addEventListener('search', fetchLocation())
